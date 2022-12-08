e3ad4fb7f80178f01724684a64bf22926819dcea
Daniil <megabaptist17@gmail.com>
2022-12-08 23:48:35 +0200

    Main Block added

---
+++ src/Main.js
@@ -0,0 +1,46 @@
+import './App.css'
+
+function Main() {
+    return (
+        //  <!-- main (start) -->
+        <section class="main" id="main">
+            <div class="container">
+                <div class="main-wrapper">
+                    <div class="offer">
+                        <div class="offer__text">
+                            <div>
+                                <h1 class="main__title">
+                                    Разрабатываю веб-сайты и&nbsp;приложения
+                                    для&nbsp;малого бизнеса
+                                </h1>
+                                <p class="main__descr">
+                                    От простых страниц до больших
+                                    интернет-магазинов. Сроки — от 5 дней.
+                                </p>
+
+                                <a
+                                    href="#contacts"
+                                    type="button"
+                                    class="button main__button"
+                                >
+                                    Тест-драйв
+                                </a>
+                            </div>
+                            <img class="offer__photo" src="#" alt="Фото" />
+                        </div>
+
+                        <a
+                            href="#design"
+                            class="main__scroll"
+                            aria-label="к следующему экрану"
+                        ></a>
+                    </div>
+                </div>
+                {/* <!-- /.main-wrapper --> */}
+            </div>
+        </section>
+        // <!-- main (end) -->
+    )
+}
+
+export default Main
